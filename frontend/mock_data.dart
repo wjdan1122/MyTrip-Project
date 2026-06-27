c7b1d26d703a535acfb390b5679fc736d53d8772
wjdan1122 <goonan115@gmail.com>
2026-06-27 19:07:27 +0300

    Add mock trip data for Riyadh itinerary

---
+++ frontend/mock_data.dart
@@ -0,0 +1,17 @@
+const Map<String, dynamic> mockTripData = {
+  "city": "Riyadh",
+  "budget": "Moderate",
+  "companions": "Friends",
+  "days": [
+    {
+      "day_number": 1,
+      "places": [
+        {"time": "09:00 AM", "name": "مقهى ومحمصة", "description": "فطور محلي وقهوة مختصة", "type": "Cafe"},
+        {"time": "11:00 AM", "name": "المتحف الوطني", "description": "جولة تاريخية وثقافية", "type": "Historical"},
+        {"time": "01:00 PM", "name": "مطعم نجدي", "description": "غداء تراثي", "type": "Restaurant"}
+        // تكملين حتى 7 أماكن
+      ]
+    }
+    // اليوم الثاني والثالث
+  ]
+};
